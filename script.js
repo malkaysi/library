550160e898c2a54f099cde2f6e72c83c11c6dedd
malkaysi <58181661+malkaysi@users.noreply.github.com>
2020-11-29 21:00:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
 let myLibrary = [];
 
-const Book = {};
+let newBook;
 
-
-/* function Book(title, author, pages, status){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+function Book(title, author, pages, status){
+    this.title = document.getElementById('title').value;
+    this.author = document.getElementById('author').value;
+    this.pages = document.getElementById('pages').value;
+    this.status = document.getElementById('status').value;
     this.info = function(){
         return title + ' by ' + author + ', ' + pages + ' pages, ' + status;
     }
-} */
-
-// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'read');
-
-function createBook(form){
-    let title = form.title.value;
-    let author = form.author.value;
-    let pages = form.pages.value;
-    let status = form.status.value;
-
-    return title, author, pages, status;
 }
 
-const newBook = new Book(title, author, pages, status);
-
-// Need input boxes to pass the title, author, pages, and status from a user
-// These parameters will be used to create a new book
-// const book = new Book(); // Add parameters in here to create the new book object that will be passed to the array
-// This book is then passed to the addBooktoLibrary function to add to the array
+function createBook(){ // Creates a book usin
+    newBook = new Book();
+    addBookToLibrary(newBook);
+}
 
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
