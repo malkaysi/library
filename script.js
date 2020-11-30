52bd37cc954a686b4fd0df03e10c5e4171b9a7ef
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2020-11-29 19:36:45 -0500

    Adding html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, status){
     this.title = title;
     this.author = author;
@@ -13,3 +15,15 @@ const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'read');
 theHobbit.info();
 
 console.log(theHobbit.info());
+
+// Need input boxes to pass the title, author, pages, and status from a user
+// These parameters will be used to create a new book
+const book = new Book(); // Add parameters in here to create the new book object that will be passed to the array
+// This book is then passed to the addBooktoLibrary function to add to the array
+
+function addBookToLibrary(theHobbit) {
+    myLibrary.push(theHobbit);
+    console.log(myLibrary);
+}
+
+console.log(myLibrary);
