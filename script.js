63522ed1a9304e65b535715def918c36146761a4
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2020-12-01 19:27:16 -0500

    Adding books to table

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 
 let newBook;
+const table = document.querySelector('table');
 
 function Book(title, author, pages, status){ // Uses input values from html
     this.title = document.getElementById('title').value;
@@ -19,7 +20,23 @@ function createBook(){ // Creates a book after clicking submit by passing input 
 
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
-    console.log(myLibrary);
-}
+    // When it's pushed, it should select the table, create a new row, and fill the row with elements
+    let tableRow = document.createElement('tr');
+    let bookTitle = document.createElement('td');
+    let bookAuthor = document.createElement('td');
+    let bookPages = document.createElement('td');
+    let bookStatus = document.createElement('td');
 
-console.log(myLibrary);
+    bookTitle.textContent = newBook.title;
+    bookAuthor.textContent = newBook.author;
+    bookPages.textContent = newBook.pages;
+    bookStatus.textContent = newBook.status;
+
+    tableRow.appendChild(bookTitle);
+    tableRow.appendChild(bookAuthor);
+    tableRow.appendChild(bookPages);
+    tableRow.appendChild(bookStatus);
+    console.log(tableRow)
+    
+    table.appendChild(tableRow);
+}
